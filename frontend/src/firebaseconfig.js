361b9d544d63ba3fba186bd9d65d627bdf2a24f0
Paul Braemer <38728930+pcbraemer@users.noreply.github.com>
2021-11-10 16:48:07 +0200

    firebase&react-setup
    Co-Authored-By: Julia045 <64480821+Julia045@users.noreply.github.com>

---
+++ frontend/src/firebaseconfig.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyA92LMgfX_wD9Z4cZtOQmgqn_wuFnHY_t0",
+    authDomain: "product-design-group-15.firebaseapp.com",
+    projectId: "product-design-group-15",
+    storageBucket: "product-design-group-15.appspot.com",
+    messagingSenderId: "255862992316",
+    appId: "1:255862992316:web:632b15f571ebc3c4198fb9"
+  };
+  const app = initializeApp(firebaseConfig);
